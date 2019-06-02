b30d2a6314549b752aeb8360a51fc20cf23fb2fe
Sandor Koncz <sandor.koncz@betssongroup.com>
2019-06-02 09:11:14 +0200

    Added local storage

--- src/app/src/store.ts
+++ src/app/src/store.ts
@@ -1,15 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import { NetworkStore } from './network.vuex';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: 'Komponent-network',
+  modules: ['network']
+})
+
 export const store = new Vuex.Store({
   modules:{
     network: NetworkStore.ExtractVuexModule( NetworkStore ),
-  }
+  },
+  plugins: 
+  [
+    vuexLocal.plugin,
+  ]
 });
 
 export const vxm = {
-  network: NetworkStore.CreateProxy( store, NetworkStore ),  
+  network: NetworkStore.CreateProxy( store, NetworkStore ),
 }
